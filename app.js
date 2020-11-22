d3771cac6f45378b2cf28eae96abcfe313380d25
saraadelalhaddad <sara.alhaddad@colorado.edu>
2020-11-22 17:37:59 +0300

    delete feature

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 
-/*** ***/
+let candies = require("./candies");
 
+/*** ***/
 app.get("/", (req, res) => {
   console.log("HELLO");
   res.json({ message: "Hello World" });
 });
 
-const candies = require("./candies");
+app.delete("/candies/:candyId", (req, res) => {
+  const { candyId } = req.params;
+  const foundCandy = candies.find((candy) => candy.id === +candyId);
+  if (foundCandy) {
+    candies = candies.filter((candy) => candy !== foundCandy);
+    res.status(204).end();
+  } else {
+    res.status(404).json({ message: "Candy not found" });
+  }
+});
+
 app.get("/candies", (req, res) => {
   res.json(candies);
 });
